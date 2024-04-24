4ab13337fab9274ce89a5b830714ed35ec162195
Pepe <jose.moreno.t@hotmail.com>
2024-04-24 16:03:20 -0600

    Primer push

--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -1,5 +1,7 @@
-import React, {useState} from 'react';
-import '../styles/mainScreen.css'; // Asegúrate de tener este archivo CSS en la misma carpeta
+import React, { useState } from "react";
+import "../styles/mainScreen.css"; // Asegúrate de tener este archivo CSS en la misma carpeta
+import Card from "./AgentCard";
+import PopUp from "./PopUp";
 
 const MainScreen = () => {
   const [isNotificationsOpen, setIsNotificationsOpen] = useState(false);
@@ -11,26 +13,93 @@ const MainScreen = () => {
   return (
     <div className="main-container">
       <div className="left-panel">
-        Panel Izquierdo
+        <PopUp>
+          <Card
+            name="Ferdi Azaoceano"
+            callTime="1:30"
+            metrica="90%"
+            ranking="No. 5"
+            restTime="1:00"
+          />
+        </PopUp>
+        <Card
+          name="Ferdi Azaoceano"
+          callTime="1:30"
+          metrica="90%"
+          ranking="No. 5"
+          restTime="1:00"
+        />
+        <Card
+          name="Ferdi Azaoceano"
+          callTime="1:30"
+          metrica="90%"
+          ranking="No. 5"
+          restTime="1:00"
+        />
+        <Card
+          name="Ferdi Azaoceano"
+          callTime="1:30"
+          metrica="90%"
+          ranking="No. 5"
+          restTime="1:00"
+        />
+        <Card
+          name="Ferdi Azaoceano"
+          callTime="1:30"
+          metrica="90%"
+          ranking="No. 5"
+          restTime="1:00"
+        />
+        <Card
+          name="Ferdi Azaoceano"
+          callTime="1:30"
+          metrica="90%"
+          ranking="No. 5"
+          restTime="1:00"
+        />
+        <Card
+          name="Ferdi Azaoceano"
+          callTime="1:30"
+          metrica="90%"
+          ranking="No. 5"
+          restTime="1:00"
+        />
+        <Card
+          name="Ferdi Azaoceano"
+          callTime="1:30"
+          metrica="90%"
+          ranking="No. 5"
+          restTime="1:00"
+        />
+        <Card
+          name="Ferdi Azaoceano"
+          callTime="1:30"
+          metrica="90%"
+          ranking="No. 5"
+          restTime="1:00"
+        />
       </div>
+
       <div className="right-panel">
         <div className="top-section">
           Sección Superior
+          <div>WAKALA</div>
         </div>
         <div className="bottom-section">
           Sección Inferior
+          <div>WAKALA</div>
         </div>
       </div>
       <button className="notifications-button" onClick={toggleNotifications}>
-        {!isNotificationsOpen ? 'Mostrar Notificaciones' : 'Ocultar Notificaciones'}
+        {!isNotificationsOpen
+          ? "Mostrar Notificaciones"
+          : "Ocultar Notificaciones"}
       </button>
       {isNotificationsOpen && (
-        <div className="notifications-panel">
-          Aquí van las notificaciones
-        </div>
+        <div className="notifications-panel">Aquí van las notificaciones</div>
       )}
     </div>
   );
-}
+};
 
 export default MainScreen;
