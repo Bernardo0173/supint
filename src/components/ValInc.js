62ddaa623e663a484081bd75b08a2c085cea1dd1
Bernardo0173 <102173762+Bernardo0173@users.noreply.github.com>
2024-05-15 15:48:37 -0600

    Se agregaron más reportes de incidencias
    Se agregaron mas reportes de incidencias para mostrar

--- src/components/ValInc.js
+++ src/components/ValInc.js
@@ -39,9 +39,60 @@ function ValInc() {
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="1">
-        <Accordion.Header>Tipo de incidencia: Interrupción del Servicio de Internet por Falla en el Nodo Principal</Accordion.Header>
+        <Accordion.Header>Tipo de incidencia: Falla de internet en zona esmeralda</Accordion.Header>
         <Accordion.Body>
-          Este reporte detalla una interrupción del servicio de internet en la zona comercial de Centro Urbano, Ciudad del Este, debido a una falla en el nodo principal de la red. La incidencia ha afectado a numerosos negocios y residentes, causando inconvenientes significativos en sus actividades diarias y operaciones comerciales. La falla se detectó el 23 de abril de 2024 a las 10:30 a.m. y se está trabajando arduamente para restablecer el servicio lo antes posible. Se mantendrá actualizada a la comunidad sobre el progreso de las reparaciones y la estimación del tiempo de recuperación del servicio.
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+              <Form.Label>Mensaje a enviar</Form.Label>
+              <Form.Control 
+                as="textarea" 
+                rows={3} 
+                value={message} 
+                onChange={(e) => setMessage(e.target.value)} 
+              />
+            </Form.Group>
+            <Button variant="primary" onClick={handleSend}>
+              {buttonText}
+            </Button>
+          </Form>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="2">
+        <Accordion.Header>Tipo de incidencia: Falla de servicio en zona Tec </Accordion.Header>
+        <Accordion.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+              <Form.Label>Mensaje a enviar</Form.Label>
+              <Form.Control 
+                as="textarea" 
+                rows={3} 
+                value={message} 
+                onChange={(e) => setMessage(e.target.value)} 
+              />
+            </Form.Group>
+            <Button variant="primary" onClick={handleSend}>
+              {buttonText}
+            </Button>
+          </Form>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="3">
+        <Accordion.Header>Tipo de incidencia: Velocidad de internet baja en zona Polanco </Accordion.Header>
+        <Accordion.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+              <Form.Label>Mensaje a enviar</Form.Label>
+              <Form.Control 
+                as="textarea" 
+                rows={3} 
+                value={message} 
+                onChange={(e) => setMessage(e.target.value)} 
+              />
+            </Form.Group>
+            <Button variant="primary" onClick={handleSend}>
+              {buttonText}
+            </Button>
+          </Form>
         </Accordion.Body>
       </Accordion.Item>
     </Accordion>
