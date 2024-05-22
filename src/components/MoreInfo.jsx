e5ed9820a14c2681b05175961f67719f3ad6fd2b
Eric <eric_manuel14@hotmail.com>
2024-05-21 22:45:57 -0600

    El front ahora es responsivo para tabletas

--- src/components/MoreInfo.jsx
+++ src/components/MoreInfo.jsx
@@ -2,36 +2,38 @@
 //Componente que permite hacer click en las tarjetas de agente, haciendo que estas desplieguen más información sobre la llamada
 import { Modal, Button } from "react-bootstrap";
 import { useState } from "react";
-import	"../styles/emotionalStyles.css";
-import { IoIosPeople } from "react-icons/io";
+import "../styles/emotionalStyles.css";
+import { BsExclamationOctagonFill } from "react-icons/bs";
 
 const MoreInfo = (props) => {
-
   const [showMore, setshowMore] = useState(false);
 
   const getInfo = () => {
-    setshowMore (!showMore);
+    setshowMore(!showMore);
   };
-  
+
   const intervene = () => {
     alert("Interviniendo");
     console.log("Interviniendo");
   };
 
-
   return (
     <div className="cardcontainer">
       <div className={props.cardStyle} onClick={getInfo}>
-          <div className="agentName">{props.Title}</div>
-          <div className="clientName"> {props.Subtitle1} </div>
-          <div className="description"> {props.Subtitle2} </div>
-          <div className="description"> {props.Subtitle3} </div>
-          <div className="description">{props.Text1}</div>
-        
+        <div className="agentName">{props.Title}</div>
+        <div className="clientName"> {props.Subtitle1} </div>
+        <div className="description"> {props.Subtitle2} </div>
+        <div className="description"> {props.Subtitle3} </div>
+        <div className="description">{props.Text1}</div>
       </div>
-      <IoIosPeople className="intervene" onClick={intervene} />
-      <Modal show={showMore} onHide={() => (setshowMore (!showMore))} size="lg" aria-labelledby="contained-modal-title-vcenter" centered >
-        
+      <BsExclamationOctagonFill className="intervene" onClick={intervene} />
+      <Modal
+        show={showMore}
+        onHide={() => setshowMore(!showMore)}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
         <Modal.Header closeButton>
           <Modal.Title>Información de la llamada</Modal.Title>
         </Modal.Header>
@@ -43,11 +45,10 @@ const MoreInfo = (props) => {
           <p>{props.Additional1}</p>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={() => (setshowMore (!showMore))}>
+          <Button variant="secondary" onClick={() => setshowMore(!showMore)}>
             Cerrar
           </Button>
         </Modal.Footer>
-
       </Modal>
     </div>
   );
--- src/components/generativeMainScreen.jsx
+++ src/components/generativeMainScreen.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "../styles/mainScreen.css"; // Asegúrate de tener este archivo CSS en la misma carpeta
 import "bootstrap/dist/css/bootstrap.min.css";
 import Stats from "./Stats";
@@ -12,7 +12,11 @@ const generativeMainScreen = () => {
   const [agentUrl, setAgentUrl] = useState(
     "http://127.0.0.1:8080/llamada/llamadasAgentes"
   );
-  
+  const [nuevaIncidencia, setNuevaIncidencia] = useState([]);
+  const [incidenciaUrl, setIncidenciaUrl] = useState(
+    "http://127.0.0.1:8080/llamada/incidenciaoalgo"
+  );
+
   const agentColor = (state) => {
     switch (state) {
       case "Positivo":
@@ -26,7 +30,7 @@ const generativeMainScreen = () => {
     }
   };
 
-  const descargar = useCallback(async () => {
+  const descargarAgentes = useCallback(async () => {
     //equvale a declarar algo estatico (MUY GENERAL)
     const response = await fetch(url);
     const data = await response.json();
@@ -54,25 +58,47 @@ const generativeMainScreen = () => {
     setAgenteNuevo(arrNuevo);
   }, [agentUrl, setAgenteNuevo]);
 
+  const descargarInicdencias =useCallback(async () =>{
+    const response = await fetch(url);
+    const data = await response.json();
+    const arrNuevo = data.map((incidencias) => {
+      const nuevaIncidencia = {
+        id:
+          incidencias.id ,
+        tipoIncidencia:incidencias.tipoIncidencia
+      };
+      return nuevaIncidencia;
+    });
+    setNuevaIncidencia(arrNuevo);
+  }, [incidenciaUrl, setIncidenciaUrl])
+
+
   //UseEffect para descargar la informacion
   useEffect(() => {
-    descargar();
-  }, [descargar]);
+    descargarAgentes();
+  }, [descargarAgentes]);
+
+
+  //UseEffect para descargar la informacion
+  useEffect(() => {
+    descargarAgentes();
+  }, [descargarAgentes]);
+
+
 
   return (
     <div className="main-container">
       <div className="left-panel">
         {agenteNuevo.map((agent) => (
           <MoreInfo
-          key={agent.name}
-          Title={agent.name}
-          Subtitle1={agent.client}
-          Subtitle2={agent.callTime}
-          Subtitle3= {agent.problemsSolved}
-          Text1={agent.description}
-          Additional1={agent.additionalInfo}
-          style={agent.style}
-
+            key={agent.name}
+            Title={agent.name}
+            Subtitle1={agent.client}
+            Subtitle2={agent.callTime}
+            Subtitle3={agent.problemsSolved}
+            Text1={agent.description}
+            Additional1={agent.additionalInfo}
+            style={agent.style}
           />
         ))}
       </div>
@@ -81,8 +107,20 @@ const generativeMainScreen = () => {
           <Stats /> {/* Aquí se renderiza el componente Stats */}
         </div>
         <div className="bottom-section">
-          <ValInc /> {/* Aquí se renderiza el componente Reporte */}
-          <ButPopMens />
+          {inc.length === 0 ? (
+            <p>No hay reportes de incidencias por el momento</p>
+          ) : (
+            <Accordion>
+              {inc.map((incidencia) => (
+                <ValInc
+                  key={incidencia.id}
+                  eventKey={incidencia.id.toString()}
+                  tipoIncidencia={incidencia.tipoIncidencia}
+                  onDelete={() => handleDelete(incidencia.id)}
+                />
+              ))}
+            </Accordion>
+          )}
         </div>
       </div>
       <NotificationsOffCanvas />
